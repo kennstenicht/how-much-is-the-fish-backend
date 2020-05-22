d25f15ce41183d08306514c24fd4dc4ff54d21d5
Christoph <christoph@ag-prop.com>
2020-05-22 22:40:44 +0200

    Clean(Migrations): Remove unused events venues join table

--- db/migrate/20200508183841_create_join_table_events_venues.rb
+++
@@ -1,8 +0,0 @@
-class CreateJoinTableEventsVenues < ActiveRecord::Migration[5.2]
-  def change
-    create_join_table :events, :venues do |t|
-      t.index :event_id
-      t.index :venue_id
-    end
-  end
-end
